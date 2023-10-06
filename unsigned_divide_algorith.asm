cd19e21bdde409fd65b696afa2db04db023f2fe2
ku9278 <120108197+ku9278@users.noreply.github.com>
2023-10-06 15:13:45 +0900

    Update unsigned_divide_algorith.asm

--- unsigned_divide_algorith.asm
+++ unsigned_divide_algorith.asm
@@ -1,38 +1,58 @@
+;32bit / 16bit
+;A(0): si:bp
+;Q(dividend): dx:ax
+;M(divisor):bx
+;cnt: cx
+;resuit: quotient:Q, remainder:A
+
 org 100h
 
-;A: dx, Q: ax, M:bx, cnt: cx
-
+mov dx, 04aaah
 mov ax, 06da9h
-mov bx, 00c38h
+mov si, 00000h
+mov bp, 00000h
+mov bx, 0000ah
 
-mov cx, 16
+mov cx, 32
 
 divide:
     shl ax, 1
     rcl dx, 1
+    rcl bp, 1
+    rcl si, 1
     
     push bx
-    push dx
-    push dx
-    sub dx, bx    ;(1)
-    test dx, dx    ;if A == 0
+    push bp
+    push si
+    push si
+    
+    sub bp, bx    ;(1)
+    jnc continue1    ;carry
+    dec si
+    
+    continue1:
+    test si, si    ;if A == 0
+    jnz continue2
+    test bp, bp
     jz success
     
+    continue2:
     pop bx
-    xor bx, dx    ;If the signs of A before and after operation (1) are the same 
+    xor bx, si    ;If the signs of A before and after operation (1) are the same 
     cmp bx, 8000h
     jc success
 
 failure:
     ;Q0:0
-    pop dx
+    pop si
+    pop bp
     pop bx
     loop divide
     jmp endd  
 
 success:
     inc ax    ;Q0:1
-    add sp, 2
+    add sp, 4
     pop bx
     loop divide
     jmp endd 
@@ -41,5 +61,3 @@ endd:
     ret
 
 end
-
-
